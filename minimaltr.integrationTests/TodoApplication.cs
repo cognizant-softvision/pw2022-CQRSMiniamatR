0cee04a2ec2039d0efd01164072a3978b99bb507
jhpaezCSV <javier.paez@softvision.com>
2022-09-12 08:59:52 -0300

    Ping test finished

--- minimaltr.integrationTests/TodoApplication.cs
+++ minimaltr.integrationTests/TodoApplication.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
+using minimalTR_dal;
 
 class TodoApplication : WebApplicationFactory<Program>
 {
@@ -15,9 +15,12 @@ class TodoApplication : WebApplicationFactory<Program>
         builder.ConfigureServices(services =>
         {
             services.RemoveAll(typeof(DbContextOptions<TodoDb>));
-
             services.AddDbContext<TodoDb>(options =>
-                options.UseInMemoryDatabase("Testing", root));
+                options.UseInMemoryDatabase("Testing_Todo", root));
+
+            services.RemoveAll(typeof(DbContextOptions<MinimaltrDB>));
+            services.AddDbContext<MinimaltrDB>(options =>
+                options.UseInMemoryDatabase("Testing_Api", root));
         });
 
         return base.CreateHost(builder);
